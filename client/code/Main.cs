ed75f0c2161509d39ec8bf66b60534e0a97bf5b4
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2022-11-30 13:55:23 +0000

    About to update to Godot 3.5 for AudioStreamPlayer use

--- client/code/Main.cs
+++ client/code/Main.cs
@@ -43,56 +43,21 @@ public class Main : Control
             }
 
         }
-
-        // Recording inputs
-        if (Input.IsActionPressed("ui_talk"))
-        {
-            if (_effect.IsRecordingActive())
-            {
-                //_recording = _effect.GetRecording();
-                //GetNode<Button>("PlayButton").Disabled = false;
-                //GetNode<Button>("SaveButton").Disabled = false;
-                //_effect.SetRecordingActive(false);
-                //GetNode<Button>("RecordButton").Text = "Record";
-                //GetNode<Label>("Status").Text = "";
-                GD.Print("Stopping...");
-            }
-            else
-            {
-                //GetNode<Button>("PlayButton").Disabled = true;
-                //GetNode<Button>("SaveButton").Disabled = true;
-                //_effect.SetRecordingActive(true);
-                //GetNode<Button>("RecordButton").Text = "Stop";
-                //GetNode<Label>("Status").Text = "Recording...";
-                GD.Print("Recording...");
-            }
-        }
-        else if (Input.IsActionPressed("ui_playback"))
-        {
-            _recording.SaveToWav("res://temp.wav");
-            //GetNode<Label>("Status").Text = string.Format("Saved WAV file to: {0}\n({1})", savePath, ProjectSettings.GlobalizePath(savePath));
-            GD.Print("Saved!");
-
-            GD.Print(_recording);
-            GD.Print(_recording.Format);
-            GD.Print(_recording.MixRate);
-            GD.Print(_recording.Stereo);
-            byte[] data = _recording.Data;
-            GD.Print(data.Length);
-            var audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-            audioStreamPlayer.Stream = _recording;
-            audioStreamPlayer.Play();
-        }
-
     }
 
     public override void _Process(float delta)
     {
+        AudioStreamPlayer audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        AudioStreamGenerator generator = new AudioStreamGenerator();
+
         Godot.Vector2[] data = _effect.GetBuffer(_effect.GetBufferLengthFrames());
         _effect.ClearBuffer();
+        
+        foreach (Vector2 frame in data)
+        {
+            generator.
+        }
 
-        var audioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-        audioStreamPlayer.Stream = data;
         audioStreamPlayer.Play();
     }
 
