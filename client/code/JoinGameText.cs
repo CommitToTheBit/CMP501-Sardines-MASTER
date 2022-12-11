e8958ebe877a83fb2b3d3e43dd6c5fabdc12e55e
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2022-12-11 12:44:32 +0000

    Typeable IP established - final step is integrating network...

--- client/code/JoinGameText.cs
+++ client/code/JoinGameText.cs
@@ -20,19 +20,50 @@ public class JoinGameText : Text
         timer.OneShot = false;
         AddChild(timer);
 
+        ip = "";
         underscore = "_";
+
         ipPseudoButton.Connect("focus_entered",timer,"start");
         ipPseudoButton.Connect("focus_exited",timer,"stop");
         ipPseudoButton.Connect("focus_entered",this,"SetUnderscore",new Godot.Collections.Array() {"_"});
         ipPseudoButton.Connect("focus_exited",this,"SetUnderscore",new Godot.Collections.Array() {""});
         timer.Connect("timeout",this,"SetUnderscore",new Godot.Collections.Array() {"!"});
         
-        ipPseudoButton.GrabFocus();//GetNode<RichTextLabel>("InternetworkPseudoButton").GrabFocus();
+        ipPseudoButton.GrabFocus();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+
+        if (!ipPseudoButton.HasFocus())
+            return;
+        
+        if (!(@event is InputEventKey) || !@event.IsPressed())
+            return;
+
+        string digit = OS.GetScancodeString(((InputEventKey)@event).Scancode);
+        if (digit.Equals("Period"))
+            digit = ".";
+
+        List<string> ipDigits = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."};
+        if (ipDigits.Contains(digit) && ip.Length < 15)
+        {
+            ip += digit;
+            SetIP();
+        }
+        else if ("BackSpace".Equals(digit) && ip.Length > 0)
+        {
+            ip = ip.Substring(0,ip.Length-1);
+            SetIP();
+        }
     }
 
     public void SetIP()
     {
-        ipPseudoButton.unencodedText = "IP "+ip+underscore;
+        ipPseudoButton.unencodedText = "IP  "+ip;
+        if (ip.Length < 15)
+            ipPseudoButton.unencodedText += underscore;
         ipPseudoButton.FormatBbcode(ipPseudoButton.HasFocus());
     }
 
@@ -48,7 +79,7 @@ public class JoinGameText : Text
 
     public void IPPressed()
     {
-        GD.Print("IP Pressed...");
+        GD.Print("IP Pressed... "+ip+"...");
     }
 
     public void BackPressed()
