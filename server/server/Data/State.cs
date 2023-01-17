f3c6f301caf32886f223b3390f4f18331a2323b1
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-01-17 00:15:06 +0000

    Continuing...

--- server/server/Data/State.cs
+++ server/server/Data/State.cs
@@ -84,7 +84,7 @@ public class State
     {
         // STEP 0: Set 'start of game'
         mode = Mode.sandbox;
-        globalStart = 0;// DateTime.UtcNow.Ticks;
+        globalStart = DateTime.UtcNow.Ticks;
         fleets = new Dictionary<Superpower, Fleet>();
 
         // STEP 1: Set player roles
